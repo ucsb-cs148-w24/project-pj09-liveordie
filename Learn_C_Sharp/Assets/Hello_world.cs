6c27434106d5ae69de80afa05e76982722a25fc3
Thomas Harbeck <tharbeck@ucsb.edu>
2024-01-19 15:35:14 -0800

    adding files

---
+++ Learn_C_Sharp/Assets/Hello_world.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello_world : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, World!");
+    }
+}
