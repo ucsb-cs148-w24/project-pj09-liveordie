e0cbbc51a058ec26a58d56af32ff8d42f9146da5
xingyelin <xingyelin@umail.ucsb.edu>
2024-01-19 15:15:45 -0800

    add Hello World

---
+++ Hello World/Assets/Script/StartButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown(){
+        Debug.Log("Hello, World!");
+        transform.localScale = transform.localScale * 0.8f;
+    }
+
+    private void OnMouseUp(){
+        transform.localScale = transform.localScale / 0.8f;
+        SceneManager.LoadScene("Game");
+    }
+}
