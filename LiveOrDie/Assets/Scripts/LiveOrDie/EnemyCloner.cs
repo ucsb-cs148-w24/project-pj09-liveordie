39a36d403776f9c2a0c7500e25980f7794b0030f
xingyelin <xingyelin@umail.ucsb.edu>
2024-01-26 13:30:25 -0800

    add enemies

---
+++ LiveOrDie/Assets/Scripts/LiveOrDie/EnemyCloner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCloner : MonoBehaviour
+{
+    public List<GameObject> points; // list of the points that enemies will appear
+    public GameObject enemy; // game object of enemy
+    public float waitTime; // wait time for an enemy to appear on the screen
+    public GameObject enemyClone; // put enemies under empty object after creating
+    
+    IEnumerator Clone(){
+        for(int i = 0; i < 50; i++){
+            yield return new WaitForSeconds(waitTime);
+            GameObject e = Instantiate(enemy.gameObject, points[Random.Range(0, points.Count - 1)].transform.position, Quaternion.identity);
+            e.transform.SetParent(enemyClone.transform);
+        }
+    }
+
+    private void Start(){
+        StartCoroutine(Clone());
+    }
+}
