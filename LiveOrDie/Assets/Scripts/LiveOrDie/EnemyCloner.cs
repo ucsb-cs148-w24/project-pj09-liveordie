045ebde5f35a120993427fa0c4196b5db96b3aca
xingyelin <xingyelin@umail.ucsb.edu>
2024-01-29 14:16:00 -0800

    Update the number of enemies

--- LiveOrDie/Assets/Scripts/LiveOrDie/EnemyCloner.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/EnemyCloner.cs
@@ -10,7 +10,7 @@ public class EnemyCloner : MonoBehaviour
     public GameObject enemyClone; // put enemies under empty object after creating
     
     IEnumerator Clone(){
-        for(int i = 0; i < 100; i++){
+        for(int i = 0; i < 20; i++){
             yield return new WaitForSeconds(waitTime);
             GameObject e = Instantiate(enemy.gameObject, points[Random.Range(0, points.Count)].transform.position, Quaternion.identity);
             e.transform.SetParent(enemyClone.transform);
