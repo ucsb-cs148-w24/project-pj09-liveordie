300fe2b2729152d40875f6cce4e141e01deff99b
Sometimes_Puzzled <rjiang@ucsb.edu>
2024-02-14 14:48:20 -0800

    frgot to add this change too

---
+++ LiveOrDie/Assets/Tests/StartScenePanelTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using static UnityEngine.UI.CanvasScaler;
+
+public class StartScenePanelTests
+{
+    [OneTimeSetUp]
+     public void LoadScene()
+     {
+         SceneManager.LoadScene("StartScene");
+     }
+     [UnityTest]
+     public IEnumerator VerifySceneLoadedCorrectly()
+     {
+        var check = GameObject.Find("StartScreenController").GetComponent<StartScreenController>();
+        Assert.That(check, Is.Not.Null);
+        var tag = "StartScreenController";
+        Assert.That(check.name, Is.EqualTo(tag));
+        yield return null;
+     }
+    [UnityTest]
+     public IEnumerator VerifyMainCameraWork()
+     {
+        var check = GameObject.Find("Main Camera");
+        Assert.That(check, Is.Not.Null);
+        var tag = "Main Camera";
+        Assert.That(check.name, Is.EqualTo(tag));
+        yield return null;
+     }
+}
