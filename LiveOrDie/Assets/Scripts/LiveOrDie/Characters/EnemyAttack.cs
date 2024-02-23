ef908f701bd93d038c132673b66ca5365620aef1
xingyelin <xingyelin@umail.ucsb.edu>
2024-02-23 12:36:45 -0800

    update enemy attack

--- LiveOrDie/Assets/Scripts/LiveOrDie/Characters/EnemyAttack.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/Characters/EnemyAttack.cs
@@ -6,8 +6,30 @@ public class EnemyAttack : MonoBehaviour
 {
     // This will destroy the enemy if it comes into contact with the player. 
     //Will need to add to the main player script and completed healthbar/UI 
+    float damage;
+    float damagePerSecond = 1f;
+    private bool isFollowingPlayer = false;
+    private Transform playerTransform;
+    void Update()
+    {
+        if (isFollowingPlayer)
+        {
+            // calculate hp of players and update
+            damage = damagePerSecond * Time.deltaTime;
+            playerTransform.GetComponent<PlayerHealth>().DecreaseHealth(damage);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isFollowingPlayer = true;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D col) {
-        if(col.gameObject.tag == "Player") {
+        if(col.gameObject.tag == "Bullet") {
             // col.gameObject.GetComponent<PlayerScript>().hurt();
             Destroy(this.gameObject);
         }
