0ebe39b252937478ac2a1549e34b62a8f27271fe
xingyelin <xingyelin@umail.ucsb.edu>
2024-02-23 13:59:29 -0800

    update enemy attack

--- LiveOrDie/Assets/Scripts/LiveOrDie/Characters/EnemyAttack.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/Characters/EnemyAttack.cs
@@ -4,34 +4,57 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    float damage;
-    float damagePerSecond = 1f;
-    private bool isFollowingPlayer = false;
-    private Transform playerTransform;
-    void Update()
-    {
-        if (isFollowingPlayer)
-        {
-            // calculate hp of players and update
-            damage = damagePerSecond * Time.deltaTime;
-            playerTransform.GetComponent<PlayerHealth>().DecreaseHealth(damage);
-        }
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            isFollowingPlayer = true;
-        }
-    }
-
     // This will destroy the enemy if it comes into contact with the player. 
     //Will need to add to the main player script and completed healthbar/UI 
-        private void OnCollisionEnter2D(Collision2D col) {
-        if(col.gameObject.tag == "Bullet") {
-            // col.gameObject.GetComponent<PlayerScript>().hurt();
-            Destroy(this.gameObject);
+    // private void OnCollisionEnter2D(Collision2D col) {
+    //     if(col.gameObject.tag == "Bullet") {
+    //         // col.gameObject.GetComponent<PlayerScript>().hurt();
+    //         Destroy(this.gameObject);
+    //     }
+    // }
+    private Transform player1Transform;
+    private Transform player2Transform;
+    private Transform currentTarget;
+    private bool isAttacking = false;
+
+    void Start()
+    {
+        // Assuming player tags are "Player1" and "Player2"
+        player1Transform = GameObject.FindGameObjectWithTag("Player1").transform;
+        player2Transform = GameObject.FindGameObjectWithTag("Player2").transform;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            currentTarget = player1Transform;
         }
+        else if (collision.gameObject.CompareTag("Player2"))
+        {
+            currentTarget = player2Transform;
+        }
+
+        if (currentTarget != null)
+        {
+            isAttacking = true;
+            StartCoroutine(AttackPlayer());
+        }
+    }
+
+    IEnumerator AttackPlayer()
+    {
+        while (isAttacking && currentTarget != null)
+        {
+            Debug.Log("Attacking");
+            // Decrease player's health by 1 each second
+            currentTarget.GetComponent<CharacterHealth>().DecreaseHealth();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    void OnDestroy()
+    {
+        StopCoroutine(AttackPlayer());
     }
 }
