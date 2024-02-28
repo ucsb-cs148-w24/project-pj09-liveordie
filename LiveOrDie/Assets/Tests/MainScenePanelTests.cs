798b422446864fefe8b273b2483d2dd0f49b4d4d
Rachel <racheljiang310@gmail.com>
2024-02-28 12:58:59 -0800

    updated testing functions

--- LiveOrDie/Assets/Tests/MainScenePanelTests.cs
+++ LiveOrDie/Assets/Tests/MainScenePanelTests.cs
@@ -52,26 +52,49 @@ public class MainScenePanelTests
         // Assertions for Existence of Players
         var check = GameObject.Find("Players");
         Assert.That(check, Is.Not.Null);
-        var players = check.GetComponentsInChildren<CharacterMovement>();
+        var players = check.GetComponentsInChildren<Player>();
+        var playerMovements = check.GetComponentsInChildren<CharacterMovement>();
+
+        Assert.That(players, Is.Not.Null);
+        Assert.That(playerMovements, Is.Not.Null);
+
         Assert.That(players.Length, Is.EqualTo(2));
+        Assert.That(playerMovements.Count, Is.EqualTo(2));
+
         Assert.That(players.Select(a => a.tag == "Player1").Where(b => b == true).Count, Is.EqualTo(1));
         Assert.That(players.Select(a => a.tag == "Player2").Where(b => b == true).Count, Is.EqualTo(1));
+        
+        Assert.That(players.Where(p => p.whichPlayer == 1).Count, Is.EqualTo(1));
+        Assert.That(players.Where(p => p.whichPlayer == 2).Count, Is.EqualTo(1));
         yield return null;
      }
      [UnityTest]
      public IEnumerator VerifyPlayerHealthBarsLoadedCorrectly()
      {
-        // var players = GameObject.Find("Players").GetComponentsInChildren<CharacterMovement>();
-        // var healthbars = players.Select(p => p.GetComponentInChildren<CharacterHealth>());
-        // Assert.That(healthbars, Is.Not.Null);
-        // Assert.That(healthbars.Count, Is.EqualTo(2));
-        // var healthLevels = healthbars.Select(h => h.getHealth()).Distinct().ToList();
-        // // assert that all healthLevels are the same at the start
-        // Assert.That(healthLevels.Count, Is.EqualTo(1));
-        // // assert that all healthLevels are at the default health level @ start
-        // Assert.That(healthLevels.FirstOrDefault, Is.EqualTo(50));
+        var players = GameObject.Find("Players").GetComponentsInChildren<Player>();
+        var healthbars = players.Select(p => p.GetComponentInChildren<CharacterHealth>());
+        Assert.That(healthbars, Is.Not.Null);
+        Assert.That(healthbars.Count, Is.EqualTo(2));
+        var healthLevels = healthbars.Select(h => h.characterHealth).Distinct().ToList();
+        // assert that all healthLevels are the same at the start
+        Assert.That(healthLevels.Count, Is.EqualTo(1));
+        // assert that all healthLevels are at the default health level @ start
+        Assert.That(healthLevels.FirstOrDefault, Is.EqualTo(50));
         yield return null;
      }
+
+    [UnityTest]
+     public IEnumerator VerifyEnemySpawnerLoadedCorrectly()
+     {
+        var enemy = GameObject.Find("Enemies");
+        Assert.That(enemy, Is.Not.Null);
+        
+        var script = enemy.GetComponent<EnemyCloner>();
+        Assert.That(script, Is.Not.Null);
+        Assert.That(script.enemySize, Is.EqualTo(10));
+        yield return null;
+     }
+
     [UnityTest]
      public IEnumerator VerifyBackgroundLoadedCorrectly()
      {
@@ -79,6 +102,22 @@ public class MainScenePanelTests
         var map = GameObject.Find("MapGrid1");
         Assert.That(tiles, Is.Not.Null);
         Assert.That(map, Is.Not.Null);
+        yield return null;
+     }
+    [UnityTest]
+     public IEnumerator VerifyAudioSceneManagerLoadedCorrectly()
+     {
+        var audio = GameObject.Find("Audio Source");
+        Assert.That(audio, Is.Not.Null);
+        yield return null;
+     }
+    [UnityTest]
+     public IEnumerator VerifyScoreBoardControllerLoadedCorrectly()
+     {
+        var score = GameObject.Find("ScoreBoardController");
+        Assert.That(score, Is.Not.Null);
+        var script = score.GetComponent<ScoreBoardController>();
+        Assert.That(script, Is.Not.Null);
         yield return null;
      }
 }
