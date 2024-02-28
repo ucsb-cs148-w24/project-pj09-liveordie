de4f604743b6a5d5eee53e8137bc0a92e5d6940d
Jason Gu <1057337859@qq.com>
2024-02-28 14:56:54 -0800

    add animation when picking drops

--- LiveOrDie/Assets/Scripts/LiveOrDie/Drop/Drop.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/Drop/Drop.cs
@@ -1,16 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Drop : MonoBehaviour
 {
-    private float backOffset = 1f;
+    // private float backOffset = 1f;
+    private float attractSpeed = 3f;
+    private float backOffSpeed = 2f;
+    private float backOffDist = 1f;
+    private CircleCollider2D col;
+
+    private void OnEnable()
+    {
+        col = GetComponent<CircleCollider2D>();
+        col.enabled = true;
+        backOffDist = 1f;
+    }
+    
+
     protected void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("PickBox"))
         {
-            TriggerEffect();
-            DestroySelf();
+            StartCoroutine(AttractedCoroutine(other));
         }
     }
 
@@ -19,10 +33,24 @@ public abstract class Drop : MonoBehaviour
     
     protected abstract void TriggerEffect();
 
-    // private void Attracted(Vector3 pos)
-    // {
-    //     Vector3 dir = this.transform.position - pos;
-    //     this.transform.position -= 
-    // }
+    IEnumerator AttractedCoroutine(Collider2D other)
+    {
+        col.enabled = false;
+        while (backOffDist > 0)
+        {
+            Vector3 backV = (other.transform.position - this.transform.position) * (backOffSpeed * Time.deltaTime);
+            this.transform.position -= backV;
+            backOffDist -=  backV.magnitude;
+            yield return new WaitForEndOfFrame();
+        }
+        while ((this.transform.position - other.transform.position).magnitude > 1f)
+        {
+            this.transform.position +=
+                (other.transform.position - this.transform.position) * (attractSpeed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+        TriggerEffect();
+        DestroySelf();
+    }
     
 }
