bc269a5d6c46208924a0f066ccade232fe73992f
Rachel <racheljiang310@gmail.com>
2024-03-10 20:51:04 -0700

    character stat updates

--- LiveOrDie/Assets/Scripts/LiveOrDie/Characters/MainCharacters/Player.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/Characters/MainCharacters/Player.cs
@@ -1,14 +1,21 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 public class Player : MonoBehaviour
 {
     public int whichPlayer; // UNIQUE ID
-    
-    //stats fields
-    public float speed = 3f; // speed of player movement
-    public float maxRadius = 5f; // max distance between players
-    public float characterHealth = 50f; // can be changed
-    public float maxHealth = 50f;
+    [HideInInspector]
+    public bool isDead;
+
+    //stats fields stored in CharacterStat
+    [HideInInspector]
+    public CharacterStat speed;
+    [HideInInspector]
+    public CharacterStat maxRadius;
+    [HideInInspector]
+    public CharacterStat characterHealth; // maybe not needed
+    [HideInInspector]
+    public CharacterStat maxHealth;
     
     //some other possible stats
     //cooldown
@@ -16,11 +23,7 @@ public class Player : MonoBehaviour
     //attackRange
     //attackDamage
     //attackSpeed
-    //defence
-
-    
-    [HideInInspector]
-    public bool isDead; // dead?
+    //defense
     private Player peer; // BEST FRIEND
     private Rigidbody2D rb; // Physics
     private DistanceJoint2D dj; // Physics
@@ -31,59 +34,37 @@ public class Player : MonoBehaviour
     public CharacterHealth healthbar;
     private CharacterMovement movement;
     // public functions
-    public Rigidbody2D getRigidBody() {return rb;}
-
-    public SpriteRenderer getSpriteRenderer() {return render;}
-    public void EnforceSensitiveState(bool enforce){
-        healthbar.setSensitiveState(enforce);
-    }
-    public void EnforceDrunkEffect(bool enforce) { 
-        movement.ChangeDrunkState(enforce);
-    }
+    public void EnforceSensitiveState(bool enforce){ healthbar.setSensitiveState(enforce); }
+    public void EnforceDrunkEffect(bool enforce) {  movement.ChangeDrunkState(enforce); }
     public void EnforceHealthEffect(string type) { 
-        if(type == "drop"){
-            healthbar.DecreaseHealth((int)characterHealth / 2);
-        }
-        else if(type == "boost"){
-            healthbar.IncreaseHealth((int)maxHealth - (int)characterHealth);
-        }
-        else{
-            Debug.Log("Wrong use of Event Listener for EnforceHealthEffect()");
-        }
+        if(type == "drop") healthbar.DecreaseHealth((int)characterHealth.Value / 2);
+        else if(type == "boost") healthbar.IncreaseHealth((int)maxHealth.Value - (int)characterHealth.Value);
+        else Debug.Log("Wrong use of Event Listener for EnforceHealthEffect()");
     }
     public void EnforceSpeedEffect(string type) { 
-        if(type == "drop"){
-            speed = 1;
-        }
-        else if(type == "boost"){
-            speed *= 1.5f;
-        }
-        else if(type == "berzerkers"){
-            speed *= 10f;
-        }
-        else{
-            Debug.Log("Wrong use of Event Listener for EnforceSpeedEffect()");
-        }
+        if(type == "drop") speed.AddModifier(new StatModifier(StatModifierType.Flat, speed.Value/2f), 0);
+        else if(type == "boost")speed.AddModifier(new StatModifier(StatModifierType.Flat, speed.Value*1.5f), 0);
+        else if(type == "berzerkers") speed.AddModifier(new StatModifier(StatModifierType.Flat, speed.Value*15f), 0);
+        else Debug.Log("Wrong use of Event Listener for EnforceSpeedEffect()");
     }
 
     // setter functions
     private void KillPlayer() {isDead = true;}
     public void ResetCharacteristics(){
-        speed = 5f;
+        speed.statModifiers.Clear();
         movement.ChangeDrunkState(false);
-        healthbar.setSentitiveState(false);
+        healthbar.setSensitiveState(false);
         EventMgr.Instance.EventTrigger("MagicMushroom", false); // reset
     }
     protected void OnEnable(){
+        speed = new CharacterStat(3f);
+        maxRadius = new CharacterStat(5f);
+        characterHealth = new CharacterStat(50f);
+        maxHealth = new CharacterStat(50f);
         isDead = false;
         if(!(rb = gameObject.GetComponent<Rigidbody2D>())) 
             rb = gameObject.AddComponent<Rigidbody2D>();
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        
-        speed = 3f; // speed of player movement
-        maxRadius = 5f; // max distance between players
-        characterHealth = 50f; // can be changed
-        maxHealth = 50f;
     }
 
     protected void Start() {
@@ -92,7 +73,7 @@ public class Player : MonoBehaviour
                 peer = GameObject.FindGameObjectWithTag("Player2").GetComponent<Player>();
                 dj = gameObject.AddComponent<DistanceJoint2D>();
                 dj.connectedBody = peer.GetComponent<Rigidbody2D>();
-                dj.distance = maxRadius;
+                dj.distance = maxRadius.Value;
                 dj.maxDistanceOnly = true;
                 break;
             case 2: // User 2 finds User 1 (left)
