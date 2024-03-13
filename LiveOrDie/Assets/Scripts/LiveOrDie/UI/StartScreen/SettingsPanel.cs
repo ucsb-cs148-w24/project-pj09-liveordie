ce34a2b08453dafc9590feb4ef055ff31d4cd298
Bgggggh <122944781+Bgggggh@users.noreply.github.com>
2024-03-13 14:34:58 -0700

    merge conflict change2 in SettingsPanel.cs

--- LiveOrDie/Assets/Scripts/LiveOrDie/UI/StartScreen/SettingsPanel.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/UI/StartScreen/SettingsPanel.cs
@@ -21,6 +21,7 @@ public class SettingsPanel : BasePanel
     {
         (transform as RectTransform).offsetMax= Vector2.zero;
         (transform as RectTransform).offsetMin= Vector2.zero;
+        
     }
     
     
