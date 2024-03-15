355ca0a6abf02263a62cf3fe228194da451ec500
xingyelin <xingyelin@umail.ucsb.edu>
2024-03-15 11:33:29 -0700

    make the tutorial

---
+++ LiveOrDie/Assets/Scripts/LiveOrDie/UI/TutorialScene/TutorialPanel.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TutorialPanel : BasePanel
+{
+    protected override void OnClick(string buttonName)
+    {
+        switch (buttonName)
+        {
+            case "BackButton":
+                print("back");
+                AudioMgr.Instance.PlayAudio("OnClick", false);
+                UIMgr.Instance.HidePanel("TutorialPanel");
+                break;
+            default:
+                break;
+        }
+    }
+    
+    public override void Show()
+    {
+        (transform as RectTransform).sizeDelta= new Vector2(960,540); //same as size in prefab
+        EventMgr.Instance.EventTrigger("GamePaused"); //**send game paused event 
+        Time.timeScale = 0;
+    }
+
+    public override void Hide()
+    {
+        Time.timeScale = 1;
+        EventMgr.Instance.EventTrigger("GameResumed"); //**send game paused event 
+    }
+
+
+    private void GoToScene(string sceneName)
+    {
+        UIMgr.Instance.ShowPanel<LoadingPanel>("LoadingPanel", E_PanelLayer.Top); //show loading panel
+        PoolMgr.Instance.Clear(); //empty the pool to prevent null reference
+        
+        SceneMgr.Instance.LoadSceneAsync(sceneName, () =>
+        {
+            EventMgr.Instance.EventTrigger("ProgressBar", 1f);
+            // EventMgr.Instance.EventTrigger("Load"+sceneName+"Completed");  //for later usage 
+            
+        }); 
+    }
+    
+}
