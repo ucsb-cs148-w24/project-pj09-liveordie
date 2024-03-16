21dfab2118da6717dd651ca5fa397c072cc8ddca
Frank404NotFound <ye_zhong@ucsb.edu>
2024-03-15 22:25:32 -0700

    Update DialogueController.cs

--- LiveOrDie/Assets/Scripts/LiveOrDie/UI/Dialogue/DialogueController.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/UI/Dialogue/DialogueController.cs
@@ -7,6 +7,7 @@ public class DialogueController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        AudioMgr.Instance.PlayBGM("StartingMenu-Brain");
         UIMgr.Instance.ShowPanel<DialoguePanel>("DialoguePanel", E_PanelLayer.Mid);
     }
 
