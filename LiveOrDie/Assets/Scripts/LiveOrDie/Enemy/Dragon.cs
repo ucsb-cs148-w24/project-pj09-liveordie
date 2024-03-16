18f2fb819ae22671a7a82e6b2dbb6ce0c78b6f5e
Jason Gu <1057337859@qq.com>
2024-03-15 19:09:39 -0700

    add melee attack and adjust hit box for dragon

--- LiveOrDie/Assets/Scripts/LiveOrDie/Enemy/Dragon.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/Enemy/Dragon.cs
@@ -8,7 +8,6 @@ using Random = UnityEngine.Random;
 
 public class Dragon : Enemy
 {
-    private Rigidbody2D rb;
     private EnemyHealth enemyHealth;
     private int points = 500; // how many points a dragon is worth
     private Vector3 moveDir;
@@ -18,14 +17,23 @@ public class Dragon : Enemy
 
     private Transform headTransform;
     private Vector3 headPosition;
+
+    private float attackCoolDownTime = 0.2f;
+
+    private int chooseTarget;
+    
+    
+    private Player player1, player2;
+    private Player currentTarget;
+    private bool canAttack = true;
     public override void Initialize() {
         health = 100;
-        damage = 500;
-        SetTarget();
+        damage = 2;
+        SetTargetDirection();
 
-        render = GetComponentsInChildren<SpriteRenderer>().ToList().Where(p => p.name=="head").FirstOrDefault();
-        tailTransform =  GetComponentsInChildren<Transform>().ToList().Where(p => p.name=="tail end").FirstOrDefault();
-        headTransform =  GetComponentsInChildren<Transform>().ToList().Where(p => p.name=="head").FirstOrDefault();
+        render = GetComponentsInChildren<SpriteRenderer>().ToList().FirstOrDefault(p => p.name=="head");
+        tailTransform =  GetComponentsInChildren<Transform>().ToList().FirstOrDefault(p => p.name=="tail end");
+        headTransform =  GetComponentsInChildren<Transform>().ToList().FirstOrDefault(p => p.name=="head");
         render.material = originalMat;
 
         //subcomponents
@@ -37,6 +45,9 @@ public class Dragon : Enemy
     void OnEnable()
     {
         Initialize();
+        player1 = GameObject.FindGameObjectWithTag("Player1").GetComponent<Player>();
+        player2 = GameObject.FindGameObjectWithTag("Player2").GetComponent<Player>();
+        canAttack = true;
     }
 
     private void Update()
@@ -46,7 +57,8 @@ public class Dragon : Enemy
         if ((tailPosition.x < 0 || tailPosition.x > 1f || tailPosition.y < 0 || tailPosition.y > 1f) &&
             (headPosition.x < 0 || headPosition.x > 1f || headPosition.y < 0 || headPosition.y > 1f)) 
         {
-            SetTarget();
+            chooseTarget = Random.Range(0, 2);
+            SetTargetDirection();
         }
 
     }
@@ -57,26 +69,13 @@ public class Dragon : Enemy
         CheckDead();
     }
 
-    private void SetTarget() {
-        int chooseTarget = Random.Range(0, 2);
+    private void SetTargetDirection() {
         if (chooseTarget == 0) target = GameObject.FindGameObjectWithTag("Player1");
         else target = GameObject.FindGameObjectWithTag("Player2");
         this.transform.right = -(target.transform.position - this.transform.position);
         
     }
-
-    // public void Attack()
-    // {
-    //     if(!this) return;
-    //     PoolMgr.Instance.GetObjAsync("Prefabs/dragon_fire", (vomit) => {
-    //         if(!vomit) return;
-    //         vomit.transform.position = (gameObject.transform.position);
-    //         vomit.transform.parent = transform;
-    //         DragonVomitAttackBehavior dragonVomitAttack = GetComponent<DragonVomitAttackBehavior>();
-    //         dragonVomitAttack.Initialize();
-    //         // dragonVomitAttack.StartAutoAttack();
-    //     });
-    // }
+    
     private void CheckDead()
     {
         if (health <= 0){
@@ -91,5 +90,35 @@ public class Dragon : Enemy
         EventMgr.Instance.EventTrigger("IncrementScore", points);
         EventMgr.Instance.EventTrigger("DropExp", this.gameObject.transform.position);
         PoolMgr.Instance.PushObj("Prefabs/Dragon",this.gameObject); //push gameObject back to pool
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!player1 || !player2 || player1.isDead || player2.isDead) return;
+        if (other.gameObject.CompareTag("Player1"))
+            currentTarget = player1;
+        else if (other.gameObject.CompareTag("Player2"))
+            currentTarget = player2;
+
+        if (currentTarget != null && canAttack )
+        {
+            currentTarget.GetComponentInChildren<CharacterHealth>().DecreaseHealth(damage);
+            
+            canAttack = false;
+            if(this.gameObject.activeSelf) StartCoroutine(AttackCoolDownCoroutine());
+        }
+        currentTarget = null;
+    }
+    
+    
+    IEnumerator AttackCoolDownCoroutine()
+    {
+        yield return new WaitForSeconds(attackCoolDownTime);
+        canAttack = true;
+    }
+
+    void OnDisable()
+    {
+        StopCoroutine(AttackCoolDownCoroutine());
     }
 }
--- LiveOrDie/Assets/Scripts/LiveOrDie/Enemy/DragonMovement.cs
+++ LiveOrDie/Assets/Scripts/LiveOrDie/Enemy/DragonMovement.cs
@@ -4,23 +4,16 @@ using UnityEngine;
 
 public class DragonMovement : MonoBehaviour
 {
-    private float speed = 10f; // move speed of enemies
+    private float speed = 6f; // move speed of enemies
     private GameObject target;
     private int chooseTarget;
     // private SpriteRenderer render;
     private Rigidbody2D rb;
-    private CircleCollider2D collide;
 
     // Start is called before the first frame update
     void OnEnable()
     {
-        // render = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
-        collide = GetComponent<CircleCollider2D>();
-        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        chooseTarget = Random.Range(0, 2);
-        if (chooseTarget == 0) target = GameObject.FindGameObjectWithTag("Player1");
-        else target = GameObject.FindGameObjectWithTag("Player2");
+
     }
 
     // Update is called once per frame
@@ -30,8 +23,6 @@ public class DragonMovement : MonoBehaviour
     }
     private void Move()
     {
-        // TrackDistance();
-        if (target == null) return;
         this.transform.position += -this.transform.right * speed * Time.deltaTime;
     }
 }
